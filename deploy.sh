5d763869c6e382b1bcf7b557588fd764a1e1a31e
LeeWoojin <lebind12@naver.com>
2021-11-25 19:00:32 +0900

    배포 shell Invalidation 추가
    캐시 삭제하고 다시 캡처하도록 수정

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 npm run build
 aws s3 sync ./out s3://makeyourpage-prod --acl public-read --profile makeyourpage
+aws cloudfront create-invalidation --profile=makeyourpage --distribution-id EWIH8DYL9OI7C --paths "/*" 
