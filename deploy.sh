3c7947250fd5c85856a69cbe4213a52103daf38e
LeeWoojin <lebind12@naver.com>
2021-11-27 18:18:51 +0900

    deploy 수정
    html 업로드 시 확장자를 제거하도록 구현

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 npm run build
-aws s3 sync ./out s3://makeyourpage-prod --acl public-read --profile makeyourpage
+cd out
+for file in $(find ./* -name "*.html" | xargs -I {} basename -s .html {});
+do
+    mv ${file}.html ${file}
+done
+cd ..
+aws s3 sync ./out s3://makeyourpage-prod --acl public-read --profile makeyourpage --content-type text/html
 aws cloudfront create-invalidation --profile=makeyourpage --distribution-id EWIH8DYL9OI7C --paths "/*" 
